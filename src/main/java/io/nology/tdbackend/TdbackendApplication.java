8817ea738ca2423d48cb6d1a72496d12d91bd1be
ronaldkwan93 <ronaldkwan93@gmail.com>
2025-05-31 01:04:30 +1000

    initial commit

---
+++ src/main/java/io/nology/tdbackend/TdbackendApplication.java
@@ -0,0 +1,13 @@
+package io.nology.tdbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/nology/tdbackend/TdbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nology.tdbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
