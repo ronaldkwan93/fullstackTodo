442aefdb388d9bc46f6154e0860afc7cbbf26fa3
ronaldkwan93 <ronaldkwan93@gmail.com>
2025-06-01 23:38:13 +1000

    adds feature: dropdown select for categories

--- frontend/src/pages/Todos.tsx
+++ frontend/src/pages/Todos.tsx
@@ -11,20 +11,27 @@ const Todos = ({ todosData, categoriesData }: TodosProps) => {
   if (!todosData || !categoriesData) return <p>Loading..</p>;
   console.log(categoriesData, "categories");
   const catList = [];
-  for(let i =0; i< categoriesData.length; i++) {
+  for (let i = 0; i < categoriesData.length; i++) {
     catList.push(categoriesData[i].title);
-    if(i === categoriesData.length -1) {
+    if (i === categoriesData.length - 1) {
       break;
     }
     catList.push(", ");
   }
   console.log(catList);
 
+
   return (
     <div>
       <h3>Task Categories</h3>
       <p>{catList}</p>
       <AddCategory />
+      <select>
+        {categoriesData.map((option)=> (
+          <option key={option.id}>{option.title}</option>
+        ))}
+      </select>
+
       {todosData.map((todo) => (
         <div key={todo.id}>
           <p>Task: {todo.title}</p>
